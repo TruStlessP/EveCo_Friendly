11b367b009d9b6bd191733ee2f367ba70d80be1a
Spswieck <Spswieck@gmail.com>
2019-06-03 20:01:42 -0400

    Revert "Client boilerplate has been implemented"
    This reverts commit 802c1a2ff495f2738b4cb3253d82185ef9b838cc.

--- client/config/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const baseConfig = require('./webpack.base.conf.js');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-
-module.exports = merge(baseConfig, {
-  mode: 'production',
-});
--- client/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { App } from '@organisms/App';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-ReactDOM.render(React.createElement(App), document.body);
